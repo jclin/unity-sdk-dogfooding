2e9ab4cc6fcaf824a682e5d6842e24abc1b82aba
Jerry Lin <jlin@skillz.com>
2020-06-09 15:29:39 -0700

    Initial commit, set up for Platinum

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    private uint score;
+
+    private void Start()
+    {
+        score = 0;
+    }
+
+    public void Random()
+    {
+        Debug.Log($"Random val={SkillzCrossPlatform.Random.Value()}");
+    }
+
+    public void PlusOne()
+    {
+        score += 1;
+        SkillzCrossPlatform.UpdatePlayersCurrentScore(score);
+    }
+
+    public void Abort()
+    {
+        SkillzCrossPlatform.AbortMatch();
+    }
+}
---
+++ Assets/Scripts/SkillzLauncher.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using SkillzSDK;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SkillzLauncher : MonoBehaviour, SkillzMatchDelegate
+{
+    public void Launch()
+    {
+        SkillzCrossPlatform.LaunchSkillz(this);
+    }
+
+    public void OnMatchWillBegin(Match matchInfo)
+    {
+        SceneManager.LoadScene("GameScene");
+
+    }
+
+    public void OnSkillzWillExit()
+    {
+    }
+}
